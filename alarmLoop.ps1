e16e2f444a83edae55aae57783a5f242b405af9b
Anh Ho <aho@pacificseafood.com>
2024-09-09 14:23:59 -0700

    create: alarm code for ps

---
+++ alarmLoop.ps1
@@ -0,0 +1,35 @@
+﻿
+function Set-Alarm {
+
+    # loop in 5 mins
+    param(
+        [int]$Hours = 0,
+        [int]$Minutes = 0,
+        [int]$Seconds = 0,
+        [bool]$Check = $true
+    
+    )
+    $currentTime = Get-Date
+    $totalSeconds = ($Hours * 3600) + ($Minutes * 60) + $Seconds
+
+    Write-Host "The current time is $currentTime "
+    Write-Host "Alarm set for $Hours hours, $Minutes minutes, and $Seconds seconds from now." -ForegroundColor Blue -BackgroundColor Yellow
+
+    while ($Check) {
+        Start-Sleep -Seconds $totalSeconds
+
+        # Play a sound (adjust the path to an existing .wav file on your system)
+        [System.Console]::Beep(432, 2000)  # Beep at 1000 Hz for 1 second
+
+        Write-Host "Wake up! Alarm is ringing!" -ForegroundColor DarkRed
+
+        # Sleep for 5 minutes before the next alarm
+        Start-Sleep -Seconds 3
+    }
+
+}
+
+
+# call function 
+
+Set-Alarm 0 2 0
---
+++ alarmNoLoop.ps1
@@ -0,0 +1,18 @@
+﻿# PowerShell Alarm Script
+
+param(
+    [int]$Hours = 0,
+    [int]$Minutes = 0,
+    [int]$Seconds = 0
+)
+
+$totalSeconds = ($Hours * 3600) + ($Minutes * 60) + $Seconds
+
+Write-Host "Alarm set for $Hours hours, $Minutes minutes, and $Seconds seconds from now."  -ForegroundColor Blue -BackgroundColor Yellow
+Start-Sleep -Seconds $totalSeconds
+
+# Play a sound (adjust the path to an existing .wav file on your system)
+[System.Console]::Beep(432, 2000)  # Beep at 1000 Hz for 1 second
+
+Write-Host "Wake up! Alarm is ringing!" -ForegroundColor DarkRed
+
